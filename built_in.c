0fa3e7beb6eff3c653dc3518a959a5f98bbc3b33
Brianoysters <brianoysters@gmail.com>
2023-12-13 10:22:40 -0800

    builtin

---
+++ built_in.c
@@ -0,0 +1,96 @@
+#include "shell.h"
+
+/**
+ * _myexit - the function exits the shell
+ * @info: information parameter
+ *  Return: 0(exit)
+ */
+
+int _myexit(info_t *info)
+{
+	int exit_check;
+
+	if (info->argv[1])
+	{
+		exit_check = _erratoi(info->argv[1]);
+		if (exit_check == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _mycd - function changes current directory
+ * @info: information parameter
+ *  Return: 0
+ */
+
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int changedir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			changedir_ret =
+				changedir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			changedir_ret = changedir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		changedir_ret =
+			changedir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		changedir_ret = changedir(info->argv[1]);
+	if (changedir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _myhelp - it changes the current directory
+ * @info: information parameter
+ *  Return: 0
+ */
+
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array);
+	return (0);
+}
